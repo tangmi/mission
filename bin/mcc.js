50dc756dba7220ffddf2fecfed15cee37516630f
Michael Tang <tangmi@uw.edu>
2013-08-31 20:04:57 -0700

    Big functionality commit

--- bin/mcc.js
+++ bin/mcc.js
@@ -2,19 +2,50 @@
 
 'use strict';
 
-var exec = require('child_process').exec;
-exec('which nginx', function(err, stdout, stderr) {
-	console.log();
-});
-
-
 var program = require('commander'),
 	def = require('../package.json'),
-	path = require('path'),
-	forever = require('forever'),
-	mcc = require(path.join(__dirname, '../lib/index'));
+	path = require('path');
+
+var daemon = require('daemonize2')
+	.setup({
+		main: path.join(__dirname, '../lib/daemon.js'),
+		name: 'mission',
+		pidfile: '/var/run/mission.pid',
+		// user: 'git',
+		// group: 'git',
+		silent: false
+	});
+
+// if (process.getuid() != 0) {
+// console.log('Expected to run as root');
+// process.exit(1);
+// }
+
+daemon
+	.on('starting', function() {
+		// console.log('Starting daemon...');
+	})
+	.on('started', function(pid) {
+		// console.log('Daemon started. PID: ' + pid);
+	})
+	.on('stopping', function() {
+		// console.log('Stopping daemon...');
+	})
+	.on('stopped', function(pid) {
+		// console.log('Daemon stopped.');
+	})
+	.on('running', function(pid) {
+		// console.log('Daemon already running. PID: ' + pid);
+	})
+	.on('notrunning', function() {
+		// console.log('Daemon is not running');
+	})
+	.on('error', function(err) {
+		// console.log('Daemon failed to start: ' + err.message);
+	});
 
 program.version(def.version);
+
 // program.usage('[path] [options]')
 // program.option('-p, --port <port>', 'set a custom port (default 9000)')
 // program.on('--help', function() {
@@ -24,50 +55,116 @@ program.version(def.version);
 // 	console.log('');
 // });
 
-program
-	.command('init')
-	.description('initializes the current directory for mcc-server')
-	.action(function(options) {
-		mcc.init(options);
-	});
-
-program
-	.command('add')
-	.description('adds a service to mcc')
-	.action(function(options) {});
-
-program
-	.command('remove')
-	.description('removes a service from mcc')
-	.action(function(options) {});
-
-program
-	.command('enable')
-	.description('enables a service in nginx and makes it "live"')
-	.action(function(options) {});
-
-program
-	.command('disable')
-	.description('removes a service from the enabled list')
-	.action(function(options) {});
-
+// process related commands
 program
 	.command('start')
-	.description('start the mission server with forever')
+	.description('start the mission daemon')
 	.action(function(options) {
-		new Error('not implemented');
-		// var mccServer = path.join(__dirname, '../lib/server/index');
-
-		// var child = new(forever.Monitor)(mccServer, {
-		// 	max: 3,
-		// 	silent: true,
-		// 	options: []
-		// });
-
-		// child.on('exit', function(a) {
-		// 	console.log(a)
-		// });
-		// child.start();
+		daemon.start();
 	});
 
+program
+	.command('stop')
+	.description('stop the mission daemon')
+	.action(function(options) {
+		daemon.stop();
+	});
+
+program
+	.command('restart')
+	.description('restart the mission daemon')
+	.action(function(options) {
+		daemon.stop(function(err) {
+			daemon.start();
+		});
+	});
+
+program
+	.command('kill')
+	.description('kill the mission daemon')
+	.action(function(options) {
+		daemon.kill();
+	});
+
+program.command('status')
+	.description('get the running status of the daemon')
+	.action(function(options) {
+		var pid = daemon.status();
+		if (pid) {
+			console.log('mission running. (PID: ' + pid + ')');
+		} else {
+			console.log('mission is not running.');
+		}
+	});
+
+
+// service related commands
+var setup = require('../lib/setup');
+program
+	.command('add <service> <hostname>')
+	.description('adds a service to mcc')
+	.action(function(service, hostname, options) {
+		setup.add(service, hostname);
+		// daemon.sendSignal("SIGUSR1");
+	});
+
+program
+	.command('remove <service>')
+	.description('removes a service from mcc')
+	.action(function(service, options) {
+		setup.remove(service);
+	});
+
+program
+	.command('enable <service>')
+	.description('enables a service in nginx and makes it live')
+	.action(function(service, options) {
+		setup.enable(service);
+	});
+
+program
+	.command('disable <service>')
+	.description('removes a service from the enabled list')
+	.action(function(service, options) {
+		setup.disable(service);
+	});
+
+program
+	.command('list')
+	.description('lists all available sites')
+	.action(function(options) {
+		console.log(setup.list());
+	});
+
+// configuration commands
+var configure = require('../lib/configure');
+program
+	.command('config [key] [value]')
+	.description('sets/gets configuration values')
+	.option('-l, --list', 'lists all set configurations')
+	.action(function(key, value, options) {
+		if (options.list) {
+			console.log(configure.list());
+		} else {
+			if (key && value) {
+				configure.set(key, value);
+			} else if (key) {
+				console.log(configure.get(key));
+			} else {
+				console.log('');
+				console.log('  Usage: mcc config[options][key][value]');
+				console.log('');
+				console.log('  Options: ')
+				console.log('');
+				console.log('    -l, --list lists configuration key value pairs');
+				console.log('');
+			}
+		}
+	});
+
+
 program.parse(process.argv);
+
+if (!program.args.length) {
+	program.help();
+}
