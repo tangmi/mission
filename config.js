09618bad7e58ff228ccd662b1307feb6b503738d
Michael Tang <tangmi@uw.edu>
2013-11-22 12:46:48 -0800

    Move things around to prep for rewrite

--- config.js
+++ config.js
@@ -2,11 +2,35 @@
 
 var path = require('path');
 
-var homedir = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
+var basedir = process.env[process.platform == 'win32' ? 'USERPROFILE' : 'HOME'];
+
+//just for testing
+basedir = './$HOME';
 
 module.exports = {
-	missionfolder: path.join(homedir, '.mission'),
-	appfolder: path.join(homedir, 'app'),
-	logfolder: path.join(homedir, 'log'),
-	pidfolder: path.join(homedir, 'pid'),
+	missionfolder: path.join(basedir, '.mission'),
+	appfolder: path.join(basedir, 'app'),
+	logfolder: path.join(basedir, 'log'),
+	pidfolder: path.join(basedir, 'pid'),
 };
+
+
+for (var key in module.exports) {
+	if (module.exports.hasOwnProperty(key)) {
+		ensureDirExists(module.exports[key]);
+	}
+}
+
+function ensureDirExists(path) {
+	var fs = require('fs'),
+		mkdirp = require('mkdirp');
+
+	if (fs.existsSync(path)) {
+		var stats = fs.statSync(path);
+		if (!stats.isDirectory()) {
+			throw new Error('path exists, but is not a directory: ' + path);
+		}
+	} else {
+		mkdirp.sync(path);
+	}
+}
